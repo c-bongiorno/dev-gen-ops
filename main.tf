6da50c27ebb417bf94e7fba798ab61dbdd2a2967
Carmelo Bongiorno <c.bongiorno@reply.it>
2025-05-28 16:27:20 +0200

    add

--- main.tf
+++ main.tf
@@ -10,10 +10,10 @@ resource "azurerm_storage_account" "sa" {
 
 
 resource "azurerm_storage_account" "test-ai" {
-  name                          = "sastestdevops002"
+  name                          = "sastestdevops-002"
   resource_group_name           = "rg-bongiorno-nit-001"
   location                      = "italynorth"
-  account_tier                  = "TEST"
+  account_tier                  = "Standard"
   account_replication_type      = "LRS"
   public_network_access_enabled = true
   https_traffic_only_enabled    = false
